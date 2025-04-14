4fd689c3c15325d3ed4b9c357504eada2bce9826
Abhishek <abhishekbmarathe@gmail.com>
2025-04-15 00:15:35 +0530

    tailwind and router integrated successfully

---
+++ EventManager/src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='font-bold w-fit m-auto mt-12 text-4xl'>
+      Welcome to Test page
+    </div>
+  )
+}
+
+export default Test
